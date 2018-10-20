a9d42c9782434993152c27de8b59c3dbfbd75447
Vaharsolta <vaha-22@mail.ru>
2018-10-20 22:49:34 +0300

    Продолжаю наброски для будущего To do list'a

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-
+<link rel="stylesheet" href="/css/main.css">
 </head>
 <body>
-
-
+<div id="container">
+<h1>To do list</h1>
+<h2>Организуйте свои задачи<br>
+И наслаждайтесь жизнью…</h2>
+<h3>Жизнь может казаться ошеломительной, но ей не обязательно такой быть. Todoist позволяет отслеживать все задачи в одном месте, чтобы вам было проще выполнять задуманное, сохраняя душевный покой.</h3>
+</div>
+<script src="/js/main.js"></script>
 </body>
 </html>
