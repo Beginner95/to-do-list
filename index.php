43a764e6601b79ddfab1fd83c21d95c910fff674
Vaharsolta <vaha-22@mail.ru>
2018-10-21 00:06:19 +0300

    Дизайн и разметка окончена

--- index.php
+++ index.php
@@ -2,13 +2,23 @@
 <html>
 <head>
 <link rel="stylesheet" href="/css/main.css">
+<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
 </head>
 <body>
 <div id="container">
 <h1>To do list</h1>
 <h2>Организуйте свои задачи<br>
 И наслаждайтесь жизнью…</h2>
-<h3>Жизнь может казаться ошеломительной, но ей не обязательно такой быть. Todoist позволяет отслеживать все задачи в одном месте, чтобы вам было проще выполнять задуманное, сохраняя душевный покой.</h3>
+<hr>
+<p class="quote">Жизнь может казаться ошеломительной, но ей не обязательно такой быть. To do list позволяет отслеживать все задачи в одном месте, чтобы вам было проще выполнять задуманное, сохраняя душевный покой.</p>
+<div class="todolist">
+    <div class="box"><input type="checkbox" name="check"></div>
+    <div class="box">Написать веб приложение To do list</div>
+    <div class="box">20.10.2018</div>
+    <div class="box"><input type="checkbox" name="check"></div>
+    <div class="box"><input type="checkbox" name="check"></div>
+</div>
 </div>
 <script src="/js/main.js"></script>
 </body>
