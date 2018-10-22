5af23a175a91c14e59268e01f0a2a203be4af7b1
Vaharsolta <vaha-22@mail.ru>
2018-10-22 21:27:03 +0300

    Добавил placeholder

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 <hr>
 <p class="quote">Жизнь может казаться ошеломительной, но ей не обязательно такой быть. To do list позволяет отслеживать все задачи в одном месте, чтобы вам было проще выполнять задуманное, сохраняя душевный покой.</p>
 <div class="addtolist">
-    <div class="add"><input type="text" id="add" name="add"></div>
+    <div class="add"><input type="text" id="add" name="add" placeholder="Введите наименование задачи"></div>
 </div>
 <div class="todolist">
     <div class="box"><input type="checkbox" name="check"></div>
