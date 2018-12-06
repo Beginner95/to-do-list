979cd42c3b56307f93421e92557b7ffd7bad73e3
Vaharsolta <vaha-22@mail.ru>
2018-12-06 20:59:59 +0300

    Перезаписал весь код, планирую все сделать максимально проще

--- js/main.js
+++ js/main.js
@@ -1,59 +1,9 @@
-(function(){
-    let tasks = {
-        current: [{
-            taskId: doId(),
-            taskContent: 'Task',
-            taskState: 'current'
-        }, 
-        {
-            taskId: doId(),
-            taskContent: 'Task two',
-            taskState: 'current'
-        }],
-        done: [{
-            taskId: doId(),
-            taskContent: 'Task three',
-            taskState: 'done'
-        }],
-        
-        get allTasks() {
-            return this.current.length + this.done.length;
-        },
-        get doneTasks() {
-            return this.done.length;
-        }
-    }
-    
-    tasksList = _('list');
-    c(tasksList);
-    
-    function doId() {
-        return 42;
-    }
-    
-    function c(str) {
-        console.log(str);
-    }
-    
-    _('add').onclick = function(){
-        //code
-    }
+var myNodelist = document.getElementsByTagName("li");
 
-    function _(el) {
-        return document.getElementById(el);
-    }
-    
-    btn = _('add');
-    
-    btn.onclick = function() {
-        /*Будем производить запись в базу данных */
-    };
-    
-    
-    function addElement()
-    {
-        
-    }
-    
-})();
-
+for (let i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
