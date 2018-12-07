7f103e0e5b64592406895a01028b7535061acfc0
Vaharsolta <vaha-22@mail.ru>
2018-12-07 17:11:27 +0300

    Реализовал возможность удаления задачи

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ for (let i = 0; i < myNodelist.length; i++) {
     span.appendChild(txt);
     myNodelist[i].appendChild(span);
 }
+
+var close = document.getElementsByClassName("close");
+
+for (let i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    let div = this.parentElement;
+    div.style.display = "none";
+  }
+}
