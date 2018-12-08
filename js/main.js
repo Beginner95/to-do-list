6e853fcb54ad4162114eacec9c3c4b7f830a011d
Vaharsolta <vaha-22@mail.ru>
2018-12-08 08:56:00 +0300

    Реализовал функцию добавления новой задачи

--- js/main.js
+++ js/main.js
@@ -23,3 +23,31 @@ list.addEventListener('click', function(e) {
         e.target.classList.toggle('checked');
     }
 }, false);
+
+function newElement() {
+    let li = document.createElement("li");
+    let inputValue = document.getElementById("myInput").value;
+    let t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    
+    if (inputValue === '') {
+        alert("You must write something!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    
+    document.getElementById("myInput").value = "";
+
+    let span = document.createElement("span");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (let i = 0; i < close.length; i++) {
+        close[i].onclick = function() {
+            let div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
