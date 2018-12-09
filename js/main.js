550e1724dabdaa678a33c7afa48a742f2330af6b
Vaharsolta <vaha-22@mail.ru>
2018-12-09 18:06:29 +0300

    Полностью переписал код

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-var myNodelist = document.getElementsByTagName("li");
+var myNodelist = getETN("li");
 
 for (let i = 0; i < myNodelist.length; i++) {
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
+    let span = cE("i");
+    span.className = "fa fa-trash-o";
     myNodelist[i].appendChild(span);
 }
 
-var close = document.getElementsByClassName("close");
+var close = getECN("close");
 
 for (let i = 0; i < close.length; i++) {
     close[i].onclick = function() {
@@ -17,7 +15,7 @@ for (let i = 0; i < close.length; i++) {
     }
 }
 
-var list = document.querySelector('ul');
+var list = qS('#list');
 list.addEventListener('click', function(e) {
     if (e.target.tagName === 'li') {
         e.target.classList.toggle('checked');
@@ -25,24 +23,26 @@ list.addEventListener('click', function(e) {
 }, false);
 
 function newElement() {
-    let li = document.createElement("li");
-    let inputValue = document.getElementById("myInput").value;
-    let t = document.createTextNode(inputValue);
-    li.appendChild(t);
+    let li = cE("li");
+    let inputValue = getId("task").value;
+    let t = cTN(inputValue);
+    let title = cE('span');
+    title.className = 'title';
+    li.className = 'box';
+    li.appendChild(title);
+    title.appendChild(t);
     
     if (inputValue === '') {
         alert("You must write something!");
     } else {
-        document.getElementById("myUL").appendChild(li);
+        getId("list").appendChild(li);
     }
     
-    document.getElementById("myInput").value = "";
+    getId("task").value = "";
 
-    let span = document.createElement("span");
-    let txt = document.createTextNode("\u00D7");
-    span.className = "close";
-    span.appendChild(txt);
-    li.appendChild(span);
+    let i = cE("i");
+    i.className = "fa fa-trash-o";
+    li.appendChild(i);
 
     for (let i = 0; i < close.length; i++) {
         close[i].onclick = function() {
@@ -50,4 +50,28 @@ function newElement() {
             div.style.display = "none";
         }
     }
+}
+
+function getId(id){
+    return document.getElementById(id);
+}
+
+function getECN(cn){
+    return document.getElementsByClassName(cn);
+}
+
+function getETN(tn){
+    return document.getElementsByTagName(tn);
+}
+
+function cE(cE){
+    return document.createElement(cE);
+}
+
+function cTN(cTN){
+    return document.createTextNode(cTN);
+}
+
+function qS(qS){
+    return document.querySelector(qS);
 }
