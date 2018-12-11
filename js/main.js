1f706bf157d8f97f90d1ded375cd50115c50b3be
Vaharsolta <vaha-22@mail.ru>
2018-12-11 10:20:39 +0300

    Написал функцию получения даты

--- js/main.js
+++ js/main.js
@@ -52,6 +52,20 @@ function newElement() {
     }
 }
 
+function formatDate(date) {
+
+  var dd = date.getDate();
+  if (dd < 10) dd = '0' + dd;
+
+  var mm = date.getMonth() + 1;
+  if (mm < 10) mm = '0' + mm;
+
+  var yy = date.getFullYear() % 100;
+  if (yy < 10) yy = '0' + yy;
+
+  return dd + '.' + mm + '.' + yy;
+}
+
 function getId(id){
     return document.getElementById(id);
 }
