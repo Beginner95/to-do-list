4d1d648d07c063733150b73a49081ef59ae2602d
Vaharsolta <vaha-22@mail.ru>
2018-12-11 11:43:10 +0300

    Удалил тестировочный код

--- index.php
+++ index.php
@@ -16,12 +16,7 @@
     <div class="add"><input type="text" id="task" name="add" placeholder="Введите наименование задачи"><button onclick="newElement()" id="add">Добавить</button></div>
 </div>
 <ul class="todolist" id="list">
-    <li class="box">
-        <input type="checkbox" name="check"> 
-        Написать веб приложение To do list 20.10.2018 
-        <input type="checkbox" name="check">
-        <i class="fa fa-trash-o" aria-hidden="true"></i>
-    </li>
+    
 </ul>
 </div>
 <script src="/js/fontawesome.min.js"></script>
