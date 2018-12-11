be6a277f4c727248541a6093f59414a4b9026d77
Vaharsolta <vaha-22@mail.ru>
2018-12-11 10:25:32 +0300

    Изменил функцию получения даты теперь получаю и время

--- js/main.js
+++ js/main.js
@@ -54,17 +54,22 @@ function newElement() {
 
 function formatDate(date) {
 
-  var dd = date.getDate();
-  if (dd < 10) dd = '0' + dd;
+    let dd = date.getDate();
+    if (dd < 10) dd = '0' + dd;
 
-  var mm = date.getMonth() + 1;
-  if (mm < 10) mm = '0' + mm;
+    let mm = date.getMonth() + 1;
+    if (mm < 10) mm = '0' + mm;
 
-  var yy = date.getFullYear() % 100;
-  if (yy < 10) yy = '0' + yy;
+    let yy = date.getFullYear();
 
-  return dd + '.' + mm + '.' + yy;
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+
+    return dd + '.' + mm + '.' + yy + ' ' + h + ':' + m + ':' + s;
 }
+
+alert(formatDate(new Date()));
 
 function getId(id){
     return document.getElementById(id);
